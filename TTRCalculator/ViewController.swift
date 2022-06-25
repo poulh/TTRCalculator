e5805f3bf3e3be68d96488280e40918fed2a3680
Poul Hornsleth <phornsleth@asquaredsoftware.com>
2022-06-25 14:37:29 -0400

    Initial Commit

---
+++ TTRCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TTRCalculator
+//
+//  Created by Poul Hornsleth on 6/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
